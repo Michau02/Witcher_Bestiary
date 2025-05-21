c65c275ea1c01e8d314cf8231c8a8e7fb83bd4ce
= <michal.kaluska@gmail.com>
2025-05-21 16:51:38 +0200

    created base project

---
+++ src/main/java/kaluska/michal/Witcher_Bestiary/WitcherBestiaryApplication.java
@@ -0,0 +1,13 @@
+package kaluska.michal.Witcher_Bestiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WitcherBestiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WitcherBestiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kaluska/michal/Witcher_Bestiary/WitcherBestiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package kaluska.michal.Witcher_Bestiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WitcherBestiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
