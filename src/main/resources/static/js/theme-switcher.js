15afbf992f557185ea350f1edb350b2fb71db8d1
= <michal.kaluska@gmail.com>
2025-07-28 14:58:28 +0200

    better theme switcher (still only background change)

--- src/main/resources/static/js/theme-switcher.js
+++ src/main/resources/static/js/theme-switcher.js
@@ -3,17 +3,23 @@ let switcher;
 document.addEventListener("DOMContentLoaded", () => {
     const savedTheme = localStorage.getItem("theme") || "dark";
     document.body.classList.add(`${savedTheme}-theme`);
+
+    const labels = document.querySelectorAll(".theme-switcher label");
     const themeRadios = document.querySelectorAll('input[name="theme"]');
+
     themeRadios.forEach(radio => {
-        if (radio.value === savedTheme) radio.checked = true;
-        console.log("ustawione");
+        if (radio.value === savedTheme) {
+            radio.checked = true;
+        }
 
         radio.addEventListener("change", () => {
-            console.log("jest zmiana");
             const selected = radio.value;
             document.body.classList.remove("light-theme", "dark-theme");
             document.body.classList.add(`${selected}-theme`);
             localStorage.setItem("theme", selected);
+
+            labels.forEach(label => label.classList.remove("checked"));
+            radio.closest("label").classList.add("checked");
         });
     });
 });
